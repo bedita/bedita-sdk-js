21f744f189458e4a546ebd0f4e21bf1f40c1c8e0
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2017-08-01 18:03:18 +0200

    fix: delete from trashcan

--- src/collections/trash.js
+++ src/collections/trash.js
@@ -13,6 +13,12 @@ export class TrashCollection extends ObjectsCollection {
         return super.post(model, options);
     }
 
+    delete(model, options = {}) {
+        options = ObjectsCollection.clone(options);
+        options.endpoint = `trash/${model.id}`;
+        return super.delete(model, options);
+    }
+
     findAll(options = {}) {
         options = ObjectsCollection.clone(options);
         options.endpoint = 'trash';
