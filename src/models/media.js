148d5fe8bbe2fec74c15fa79b8675d858b132c6a
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2018-02-12 12:58:10 +0100

    refactor: do not use definitions

--- src/models/media.js
+++ src/models/media.js
@@ -2,45 +2,40 @@ import { ObjectModel } from './object.js';
 import { RELATIONSHIP_MODES } from '../collections/relationships.js';
 
 const SCHEMA = {
-    definitions: ObjectModel.schema.definitions,
     type: 'object',
-    allOf: [
-        { $ref: '#/definitions/object' },
-        {
-            type: 'object',
-            properties: {
-                name: {
-                    oneOf: [
-                        { type: 'null' },
-                        { type: 'string', maximum: 255 },
-                    ],
-                },
-                width: {
-                    oneOf: [
-                        { type: 'null' },
-                        { type: 'number' },
-                    ],
-                },
-                height: {
-                    oneOf: [
-                        { type: 'null' },
-                        { type: 'number' },
-                    ],
-                },
-                duration: {
-                    oneOf: [
-                        { type: 'null' },
-                        { type: 'number' },
-                    ],
-                },
-            },
+    properties: {
+        name: {
+            oneOf: [
+                { type: 'null' },
+                { type: 'string', maximum: 255 },
+            ],
         },
-    ],
+        width: {
+            oneOf: [
+                { type: 'null' },
+                { type: 'number' },
+            ],
+        },
+        height: {
+            oneOf: [
+                { type: 'null' },
+                { type: 'number' },
+            ],
+        },
+        duration: {
+            oneOf: [
+                { type: 'null' },
+                { type: 'number' },
+            ],
+        },
+    },
 };
 
 export class MediaModel extends ObjectModel {
     static get schema() {
-        return SCHEMA;
+        return {
+            allOf: [ObjectModel.schema, SCHEMA],
+        };
     }
 
     static get relationships() {
