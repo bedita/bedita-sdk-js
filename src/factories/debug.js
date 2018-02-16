4aa5e8cf864edd6547fb19acf2d2966ac10649c8
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2018-02-16 15:47:34 +0100

    fix: do not color logs in node env

--- src/factories/debug.js
+++ src/factories/debug.js
@@ -6,14 +6,26 @@ let styles = 'font-family: monospace; color: #BBB;';
 
 export class Debug extends Factory {
     log(...args) {
-        console.log('%c [BEDITA]', styles, ...args);
+        if (typeof window !== 'undefined') {
+            console.log('%c [BEDITA]', styles, ...args);
+        } else {
+            console.log('[BEDITA]', ...args);
+        }
     }
 
     warn(...args) {
-        console.warn('%c [BEDITA]', styles, ...args);
+        if (typeof window !== 'undefined') {
+            console.warn('%c [BEDITA]', styles, ...args);
+        } else {
+            console.warn('[BEDITA]', ...args);
+        }
     }
 
     error(...args) {
-        console.error('%c [BEDITA]', styles, ...args);
+        if (typeof window !== 'undefined') {
+            console.error('%c [BEDITA]', styles, ...args);
+        } else {
+            console.error('[BEDITA]', ...args);
+        }
     }
 }
