c89886db74e196666f45b367e075c6b0f72d5953
xho <xhoxho@gmail.com>
2018-02-21 16:39:48 +0100

    refactor: always include roles in user model

--- src/models/user.js
+++ src/models/user.js
@@ -278,6 +278,7 @@ export class UserModel extends ObjectModel {
         return {
             roles: {
                 types: ['roles'],
+                include: true,
             },
         };
     }
