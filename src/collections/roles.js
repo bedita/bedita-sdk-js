c8736b84a0efeca69c82d679c47d47fe6210c86f
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2018-02-26 14:06:05 +0100

    feat: add some utils methods to roles collection

--- src/collections/roles.js
+++ src/collections/roles.js
@@ -1,12 +1,42 @@
 import { Collection } from '../collection.js';
 import { RoleModel } from '../models/role.js';
 
+/**
+ * @class RolesCollection
+ * @extends Collection
+ * Handle roles.
+ */
 export class RolesCollection extends Collection {
+    /**
+     * @inheritdoc
+     * Use RoleModel as default Model constructor.
+     */
     static get Model() {
         return RoleModel;
     }
 
+    /**
+     * @inheritdoc
+     */
     getMinimalPropertiesSet() {
         return Promise.resolve(['id', 'name']);
+    }
+
+    /**
+     * Retrieve a role by id.
+     * @param {Number} roleId
+     * @return {RoleModel}
+     */
+    getById(roleId) {
+        return this.array.find((role) => role.id == roleId);
+    }
+
+    /**
+     * Retrieve a role by name.
+     * @param {String} roleName
+     * @return {RoleModel}
+     */
+    getByName(roleName) {
+        return this.array.find((role) => role.name === roleName);
     }
 }
--- src/collections/users.js
+++ src/collections/users.js
@@ -1,11 +1,23 @@
 import { Collection } from '../collection.js';
 import { UserModel } from '../models/user.js';
 
+/**
+ * @class UsersCollection
+ * @extends Collection
+ * Handle users.
+ */
 export class UsersCollection extends Collection {
+    /**
+     * @inheritdoc
+     * Use UserModel as default Model constructor.
+     */
     static get Model() {
         return UserModel;
     }
 
+    /**
+     * @inheritdoc
+     */
     getMinimalPropertiesSet() {
         return Promise.resolve(['id', 'email', 'title', 'username', 'last_login']);
     }
